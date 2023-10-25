2ce4b0605a59000aa5c96f7ff3727d6b826dd454
fauruz <le.phong1456@gmail.com>
2023-10-25 15:31:18 +0700

    bai 1

---
+++ b1/ConnectPerson.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace b1
+{
+    internal class ConnectPerson
+    {
+        public bool addData(Person ps)
+        {
+            string constr = ConfigurationManager.ConnectionStrings["conStr"].ConnectionString;
+            SqlConnection con = new SqlConnection(constr);
+            con.Open();       
+            try
+            {
+                string sql5 = "insert into dbo.Person values('" + ps.Id + "',N'" + ps.Name + "')";
+                SqlCommand cmd5 = new SqlCommand(sql5, con);
+                cmd5.ExecuteNonQuery();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Duplicate Id !");
+                return false;
+            }
+                
+            //string sql = "select count(*) from Employee where EmpId = '" + ps.Id + "';";
+            //SqlCommand cmd = new SqlCommand(sql, con);
+            //cmd.CommandType = CommandType.Text;
+            //kt = (int)cmd.ExecuteScalar();
+            //if (kt == 0)
+            //{
+            //    string sql2 = "insert into dbo.Employee values('" + emp.Id + "',N'" + emp.Name + "','" + emp.Gender + "','" + id + "')";
+            //    SqlCommand cmd2 = new SqlCommand(sql2, con);
+            //    cmd2.ExecuteNonQuery();
+            //}
+            con.Close();
+            return true;
+        }
+        public string getRow()
+        {
+            string b = "";
+            string constr = ConfigurationManager.ConnectionStrings["conStr"].ConnectionString;
+            SqlConnection con = new SqlConnection(constr);
+            SqlDataAdapter da = new SqlDataAdapter();
+            DataTable dt = new DataTable();
+            try
+            {
+                con.Open();
+                da.SelectCommand = new SqlCommand();
+                da.SelectCommand.CommandText = "select Name from Person";
+                da.SelectCommand.Connection = con;
+                da.Fill(dt);
+                foreach (DataRow row in dt.Rows)
+                {
+                    b += row["Name"].ToString();
+                    b += "\n";
+                }
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return b;
+        }
+        public List<Person> getData()
+        {
+            List<Person> lps = new List<Person>();
+            string constr = ConfigurationManager.ConnectionStrings["conStr"].ConnectionString;
+            SqlConnection con = new SqlConnection(constr);
+            SqlDataAdapter da = new SqlDataAdapter();
+            DataTable dt = new DataTable();
+            try
+            {
+                con.Open();
+                da.SelectCommand = new SqlCommand();
+                da.SelectCommand.CommandText = "select * from Person";
+                da.SelectCommand.Connection = con;
+                da.Fill(dt);
+                foreach (DataRow row in dt.Rows)
+                {
+                    Person ps = new Person();
+                    ps.Id = Convert.ToInt32(row["Id"].ToString());
+                    ps.Name = row["Name"].ToString();
+                    lps.Add(ps);
+                }
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return lps;
+        }
+        public DataTable getTable()
+        {
+            string constr = ConfigurationManager.ConnectionStrings["conStr"].ConnectionString;
+            SqlConnection con = new SqlConnection(constr);
+            SqlDataAdapter da;
+            DataTable dt = new DataTable();
+            try
+            {
+                con.Open();
+                string sql = "SELECT * from Person";
+                da = new SqlDataAdapter(sql, con);
+                da.Fill(dt);
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return dt;
+        }
+        public bool updateData(Person ps)
+        {
+            int kt = 0;
+            string constr = ConfigurationManager.ConnectionStrings["conStr"].ConnectionString;
+            SqlConnection con = new SqlConnection(constr);
+            con.Open();
+            string sql = "select count(*) from Person where Id = " + ps.Id;
+            SqlCommand cmd = new SqlCommand(sql, con);
+            kt = (int)cmd.ExecuteScalar();
+            if(kt == 0)
+            {
+                addData(ps);
+            }
+            else
+            {
+                try
+                {
+                    string sql1 = "Update Person" +
+                        " Set Name = '" + ps.Name + "' where Id = " + ps.Id + ";";
+                    SqlCommand cmd1 = new SqlCommand(sql1, con);
+                    cmd1.ExecuteNonQuery();
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    return false;
+                }
+            }
+            con.Close() ;
+            return true;
+        }
+        public bool deleteData(Person ps)
+        {
+            int kt = 0;
+            string constr = ConfigurationManager.ConnectionStrings["conStr"].ConnectionString;
+            SqlConnection con = new SqlConnection(constr);
+            con.Open();
+            string sql = "select count(*) from Person where Id = " + ps.Id;
+            SqlCommand cmd = new SqlCommand(sql, con);
+            kt = (int)cmd.ExecuteScalar();
+            if (kt == 0)
+            {
+                MessageBox.Show("Không tồn tại Id!");
+                return false;
+            }
+            else
+            {
+                try
+                {
+                    string sql1 = "Delete From Person" +
+                        " where Id = " + ps.Id + ";";
+                    SqlCommand cmd1 = new SqlCommand(sql1, con);
+                    cmd1.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    return false;
+                }
+            }
+            con.Close();
+            return true;
+        }
+    }
+}
---
+++ b1/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace b1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public bool Require()
+        {
+            if (txtId.Text == "")
+            {
+                MessageBox.Show("Enter Id!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtId.Focus();
+                return false;
+            }
+            if (txtName.Text == "")
+            {
+                MessageBox.Show("Enter Name!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtName.Focus();
+                return false;
+            }
+            return true;
+        }
+        private Person getPerson()
+        {
+            Person ps = new Person();
+            ps.Id = Convert.ToInt32(txtId.Text);
+            ps.Name = txtName.Text;
+            return ps;
+        }
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            if (Require())
+            {
+                ConnectPerson obj = new ConnectPerson();
+                Person ps = getPerson();
+                if (obj.addData(ps))
+                    MessageBox.Show("Created!");
+                Refresh();
+                btnRead_Click(sender, e);
+            }
+        }
+        private void Refresh()
+        {
+            txtId.Text = string.Empty;
+            txtName.Text = string.Empty;
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            ConnectPerson obj = new ConnectPerson();
+            lblName.Text = obj.getRow();
+            dataGridView1.DataSource = obj.getTable();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (Require())
+            {
+                ConnectPerson obj = new ConnectPerson();
+                Person ps = getPerson();
+                if (obj.updateData(ps))
+                    MessageBox.Show("Updated!");
+                Refresh();
+                btnRead_Click(sender, e);
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (txtId.Text == "")
+            {
+                MessageBox.Show("Enter Id!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtId.Focus();
+            }
+            else
+            {
+                ConnectPerson obj = new ConnectPerson();
+                Person ps = getPerson();
+                if (obj.deleteData(ps))
+                    MessageBox.Show("Deleted!");
+                Refresh();
+                btnRead_Click(sender, e);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ConnectPerson obj = new ConnectPerson();
+            List<Person> lps = obj.getData();
+            foreach (Person p in lps)
+            {
+                cbbName.Items.Add(p.Name);
+            }
+        }
+    }
+}
